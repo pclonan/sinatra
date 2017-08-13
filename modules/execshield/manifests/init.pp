ad41d1b462d8787f1a81c037a58844acce0cb9d2
root <root@localhost.localdomain>
2017-08-13 06:35:10 -0400

    Include execshield

--- modules/execshield/manifests/init.pp
+++ modules/execshield/manifests/init.pp
@@ -1,21 +1,14 @@
 class execshield {
 
-  augeas::basic_change {"Enable ExecShield, 2.2.4.3": 
-    file=>"/etc/sysctl.conf", 
-    lens=>"sysctl.lns", 
-    changes => [
-      "set kernel.randomize_va_space 2", 
-    ]
-  }
   augeas { "sysctl":
     context => "/files/etc/sysctl.conf",
     changes => [
       "set kernel.exec-shield = 1",
-  #and whatever other lines are interesting to you
     ],
   }
   file { 'sysctl_conf':
     ensure => file,
+    path => '/etc/sysctl.conf',
     owner => 'root',
     group => 'root',
     mode => '0644',
@@ -25,5 +18,4 @@ class execshield {
       refreshonly => true,
       subscribe => File["sysctl_conf"],
    }
-
 }
