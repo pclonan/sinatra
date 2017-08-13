e08dc2f47a776bf77d98178c7fc4320eeaab1946
root <root@localhost.localdomain>
2017-08-13 05:58:42 -0400

    Create ssh module

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -14,11 +14,6 @@ class ssh {
   context => "/files/etc/ssh/sshd_config",
     changes => [
     "set PermitRootLogin no",
-    "set RSAAuthentication yes",
-    "set PubkeyAuthentication yes",
-    "set AuthorizedKeysFile	%h/.ssh/authorized_keys",
-    "set PasswordAuthentication no",
-    "set Port 22",
   ],
   notify => Service['sshd'],
  }
