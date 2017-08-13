f2cc67da72a680f959d19e94ab49284f23d5f9e5
pclonan <pclonan@hotmail.com>
2017-08-13 15:00:51 +1000

    Create user "sinatra"

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -37,5 +37,22 @@
 #
 class users {
 
+  group { 'sinatra':
+    ensure => present,
+	gid => '500',
+  }
+
+  file { '/apps':
+    ensure => directory,
+  }
+  
+  user { 'sinatra':
+    ensure => present,
+	uid => '500',
+	gid => '500',
+	home => '/apps/simple-sinatra-app',
+	password => '$1$E.xtE53M$3X7z418n5EKwXksH6C3Ey.',
+	shell => '/bin/bash',
+  }
 
 }
