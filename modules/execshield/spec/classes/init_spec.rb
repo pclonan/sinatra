65c6b5fc164fea30ee02d5f92ec37f30a2f6ba00
pclonan <pclonan@hotmail.com>
2017-08-13 21:03:42 +1000

    Combine in to osharden

--- modules/execshield/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'execshield' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('execshield') }
-  end
-end
--- modules/ipv6/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'ipv6' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('ipv6') }
-  end
-end
